b0a97ba5dcb7af64ef00a532d79ed7409ffd883d
Manoj Patra <patra.manoj0@gmail.com>
2024-09-09 17:01:40 +0530

    add logic to create new user

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,31 @@
-const NewUser = () => {
-  return <div>hi</div>
+import { db } from "@/prisma/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  const match = await db.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  });
+
+  if (!match) {
+    await db.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+
+  redirect("/journal");
+};
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div>...loading</div>;
 };
 
 export default NewUser;
