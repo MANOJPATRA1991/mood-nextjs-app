509e857b99a3e701e60d6c3ae19e1bf700b64977
Manoj Patra <patra.manoj0@gmail.com>
2024-09-09 15:54:05 +0530

    add type check for prisma

--- prisma/db.ts
+++ prisma/db.ts
@@ -3,10 +3,19 @@ import { createClient } from "@libsql/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 const adapter = new PrismaLibSQL(client);
-export const db = new PrismaClient({ adapter });
+export const db =
+  globalForPrisma.prisma ?? new PrismaClient({ adapter, log: ["query"] });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db;
+}
