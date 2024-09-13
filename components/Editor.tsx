85858ebc59b7196207dc6190c257235aa6af002f
Manoj Patra <patra.manoj0@gmail.com>
2024-09-13 08:47:24 +0530

    update EditorComponent

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,34 +1,83 @@
 "use client";
 
-import { updateEntry } from "@/utils/api";
-import { JournalEntry } from "@prisma/client";
+import { TJournalEntry } from "@/types";
+import { deleteEntry, updateEntry } from "@/utils/api";
+import { EntryAnalysis } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useAutosave } from "react-autosave";
 
-type TProps = { entry: JournalEntry };
-
-const Editor = ({ entry }: TProps) => {
+const Editor = ({ entry }: TJournalEntry) => {
   const [value, setValue] = useState(entry.content);
+  const [currentAnalysis, setAnalysis] = useState<EntryAnalysis>(entry.analysis);
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const analysisData = [
+    { name: "Summary", value: currentAnalysis.summary },
+    { name: "Subject", value: currentAnalysis.subject },
+    { name: "Mood", value: currentAnalysis.mood },
+    { name: "Negative", value: currentAnalysis.negative },
+  ];
+
+  const handleDelete = async () => {
+    await deleteEntry(entry.id);
+    router.push("/journal");
+  };
 
   useAutosave({
     data: value,
     onSave: async (_value) => {
+      console.log({ _value })
       if (_value === entry?.content) return;
       setIsLoading(true);
-      const updated = await updateEntry(entry.id, _value);
+      const data = await updateEntry(entry.id, _value);
+      console.log({ data })
+      setAnalysis(data);
       setIsLoading(false);
     },
-  })
+  });
 
   return (
-    <div className="w-full h-full">
-      {isLoading && (<div>...loading</div>)}
-      <textarea
-        className="w-full h-full text-xl p-8 outline-none"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+    <div className="w-full h-full grid grid-cols-3">
+      {isLoading && <div>...loading</div>}
+      <div className="col-span-2">
+        <textarea
+          className="w-full h-full text-xl p-8 outline-none"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <div className="border-l border-black/5">
+        <div
+          style={{ background: currentAnalysis.color }}
+          className="px-6 py-10"
+        >
+          <h2 className="text-2xl bg-white/25 text-black">Analysis</h2>
+        </div>
+        <div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value.toString()}</span>
+              </li>
+            ))}
+            <li className="py-2 px-4 flex items-center justify-between">
+              <button
+                onClick={handleDelete}
+                type="button"
+                className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              >
+                Delete
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
